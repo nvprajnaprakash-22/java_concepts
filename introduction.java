51c91dcb561dc6336327ea69f5a197c58f344f91
nvprajnaprakash-22 <nvprajnaprakash@gmail.com>
2026-03-31 22:12:59 +0530

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,6 @@
+public class introduction{
+    public static void main(String [] str){
+        System.out.println("This is my first ever java programme !!");
+        System.out.println("I am starting my journey in java ocean to learn and understand all about java ");
+    }
+}
